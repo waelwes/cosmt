b7152789ba755b40ec4de9f712fadbc9bcf9eafc
waelwes <wael.silver@gmail.com>
2025-10-18 21:04:36 +0300

    fix: Correct import paths in mom-baby-care page

--- app/categories/mom-baby-care/page.tsx
+++ app/categories/mom-baby-care/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import React from 'react';
-import { PageLayout } from '../../components/layout/PageLayout';
-import { ProductListingPage } from '../../components/pages/ProductListingPage';
+import { PageLayout } from '../../../components/layout/PageLayout';
+import { ProductListingPage } from '../../../components/pages/ProductListingPage';
 
 export default function MomBabyCarePage() {
   return (
